053b3c35967b7927fd4b5242868d673f0d4cf249
Baiya1117 <126648341+Baiya1117@users.noreply.github.com>
2023-06-02 05:52:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include "Move.h"
+#include "Block.h"
+#include "Board.h"
 #include "Game.h"
 
 int main() {
-    std::cout << "Tetris Game\n";
 
-    Game game;  // 創建遊戲對象
-    game.start();  // 開始遊戲
+    Game start_game;
 
     return 0;
 }
