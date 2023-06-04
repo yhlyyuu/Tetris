3adc33d0c689c90c3b2a5a8b1b4b354dbad9ac77
yhlyyuu <126648746+yhlyyuu@users.noreply.github.com>
2023-06-04 21:07:01 +0800

    Add files via upload

---
+++ Block.h
@@ -0,0 +1,45 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include "Move.h"
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+enum piece_type {         //方塊的種類
+    t_piece,
+    i_piece,
+    o_piece,
+    l_piece,
+    j_piece,
+    s_piece,
+    z_piece
+};
+
+enum move_direction {   //移動方向
+    rightO = 1,
+    leftO = -1
+};
+class Block {
+private:
+    vector<Move> body;//各個方塊單元的位置
+    Move pos;//方塊的當前位置
+    piece_type type;
+
+public:
+    Block();
+    Block(Move pos);//初始化方塊的位置
+    void fall_down();//方塊向下移動(更新方塊的位置)
+    vector<Move> next_fall_down_body();//垂直方向移動一格
+    void move(move_direction dir);//水平方向
+    vector<Move> next_move_body(move_direction dir);
+    void rotate();//方塊旋轉
+    vector<Move> next_rotate_body();//方塊進行旋轉操作後的位置
+
+    vector<Move> get_body() { return body; }//返回方塊的位置
+    void set_body(vector<Move> b) { body = b; }//設定方塊的位置
+};
+
+
+#endif
